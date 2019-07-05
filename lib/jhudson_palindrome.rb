5ffd2da44f4519bd45ad1f15b245a1fac36aa579
James <jwhudson6@gmail.com>
2019-07-05 12:00:36 +0100

    Initialise Repository

---
+++ lib/jhudson_palindrome.rb
@@ -0,0 +1,6 @@
+require "jhudson_palindrome/version"
+
+module JhudsonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jhudson_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JhudsonPalindrome
+  VERSION = "0.1.0"
+end
